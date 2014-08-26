fefd93c1dfd94d77b6e54facbb5101dc2c9008f1
Mandy <mandy.thomson@telus.com>
2014-08-26 09:02:48 -0400

    instagram works

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
 $(function() {
-    
+  var feed = new Instafeed({
+          get: 'tagged',
+          tagName: 'dogs',
+          clientId: '876cebe25a23446fa882f160f4327a9b',
+          useHttp: true, 
+          sortBy: 'most-recent',
+          resolution: 'low_resolution',
+          template: '<a class="fancybox" rel="gallery" title="{{caption}}" data-photo="{{model.user.profile_picture}}" href="{{model.images.standard_resolution.url}}"><img src="{{image}}" /></a>'
+      });
+      feed.run();
 
-
-
-
+      // fancybox
+     $(".fancybox").fancybox({
+    afterLoad: function() {
+        this.title = this.title + '<img class="profile" src"">';
+    },
+    helpers : {
+        title: {
+            type: 'inside'
+        }
+    }
+});
+      var profile = $('.fancybox').data('photo');
+      $('img.profile').attr('src', profile);
     
 }); // end of doc ready
